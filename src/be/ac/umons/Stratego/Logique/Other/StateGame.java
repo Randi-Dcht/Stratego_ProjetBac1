7a02320c059c3f65cd0f2bcb0fe0d755bc25fc92
190134 <190134@umons.ac.be>
2020-01-22 11:40:18 +0100

    add the code of 'other' class

--- src/be/ac/umons/Stratego/Logique/Other/StateGame.java
+++ src/be/ac/umons/Stratego/Logique/Other/StateGame.java
@@ -1,4 +1,34 @@
 package Stratego.Logique.Other;
 
-public class StateGame {
+import Stratego.Logique.Player.Player;
+
+public class StateGame
+{
+    /**
+     *Cette méthode permet de voir si le drapeau a été attrapé par un des adversaires.
+     @param joueurs qui est une liste des joueur qui joue la partie.
+     @return true si le drapeau est attrapé sinon false.
+     */
+    public static boolean endGame(Player[] joueur)
+    {
+        int cnt =0;
+        for(int j=0; j < joueur.length ; j++ )
+        {
+            if (!joueur[j].myList()[0].vivant)
+            {
+                //Summary.WRITE("endGame : la partie est terminé, drapeau de " + joueur[j].myName() + " est attrapé");
+                return true;
+            }
+            if(joueur[j].howMany() == 0)
+            {
+                cnt++;
+            }
+        }
+        if(cnt == joueur.length)
+        {
+            //Summary.WRITE("endGame : la partie est terminé, il n'y a plus de pièces déplaçables");
+            return true;
+        }
+        return false;
+    }
 }
