aa5f16c51d4509dd324f43c798b0b80e044ab23a
190134 <190134@umons.ac.be>
2020-01-22 11:44:51 +0100

    add the code of Player class

--- src/be/ac/umons/Stratego/Logique/Player/Couleur.java
+++ src/be/ac/umons/Stratego/Logique/Player/Couleur.java
@@ -1,4 +1,22 @@
 package Stratego.Logique.Player;
 
-public class Couleur {
+public class Couleur
+{
+    final int rouge;
+    final int vert;
+    final int bleu;
+    final String nom;
+
+    public Couleur(int rg , int vt , int bl , String nom_)
+    {
+        rouge =  rg;
+        vert  =  vt;
+        bleu  =  bl;
+        nom   =  nom_;
+    }
+
+    public String toString()
+    {
+        return nom;
+    }
 }
--- src/be/ac/umons/Stratego/Logique/Player/Player.java
+++ src/be/ac/umons/Stratego/Logique/Player/Player.java
@@ -1,4 +1,17 @@
 package Stratego.Logique.Player;
 
-public class Player {
+import Stratego.Logique.Pawn.Piece;
+
+public interface Player
+{
+    void assignment(Player joueur, Piece[] liste, int nombrePiece);
+    Player next();
+    void dead();
+    Couleur myColor();
+    int howMany();
+    Piece[] myList();
+    int[] wherePlace();
+    String myName();
+    void catchOther(Piece piece);
+    Piece[] captureList();
 }
